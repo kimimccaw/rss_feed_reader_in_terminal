d452fcb747787b74ca843c3f6bba82bff73e2847
kimimccaw <amirhakimibin.azman@gmail.com>
2021-04-08 11:03:36 +0200

    added main method added option to add multiple link and '0' to quit program

--- rss_feed_reader.py
+++ rss_feed_reader.py
@@ -12,12 +12,24 @@ class feed_parsing:
     def get_feed_link(self):
         return self.d.feed.link
 
-if __name__ == "__main__":
-    url = input("Insert your link: ")
+def main(url):
     link = feed_parsing()
     link.feed_parser(url)
 
-
-    print("RSS feed Title: ",link.get_feed_title())
+    print("\n\nRSS feed Title: ",link.get_feed_title())
     print("Feed Description: " ,link.get_feed_description())
     print("Original Link: " ,link.get_feed_link())
+
+
+    
+
+if __name__ == "__main__":
+    url = str(input("Add url: "))
+
+    while url != str(0) :
+                
+        main(url)
+        print("\n\nadd 0 to stop \n\n")
+        url = str(input("Add url: "))
+        if url == str(0):
+            print("Program exit")
